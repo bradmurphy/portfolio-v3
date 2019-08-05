6d0d569409fbde73450c46951ef276964d269888
Brad Murphy <brad@blackairplane.com>
2019-08-04 23:23:31 -0400

    Updating background styles.

--- src/components/Work.js
+++ src/components/Work.js
@@ -6,24 +6,22 @@ class Work extends Component {
     render() {
         return (
             <section className="component-work">
-                <div className="component-work__wrap">
-                    <h2 className="typography__work-headline">{`//`} <em>Work</em></h2>
-                    {
-                        data.map((project, key) => (
-                            <Project
-                                date={project.date}
-                                headline={project.headline}
-                                key={key}
-                                src={project.logo}
-                                srcAlt={project.headline}
-                                tech={project.technology}
-                                type={project.type}
-                                url={project.url}
-                                width={project.width}
-                            >{ project.copy }</Project>
-                        ))
-                    }
-                </div>
+                <h2 className="typography__work-headline">{`//`} <em>Work</em></h2>
+                {
+                    data.map((project, key) => (
+                        <Project
+                            date={project.date}
+                            headline={project.headline}
+                            key={key}
+                            src={project.logo}
+                            srcAlt={project.headline}
+                            tech={project.technology}
+                            type={project.type}
+                            url={project.url}
+                            width={project.width}
+                        >{ project.copy }</Project>
+                    ))
+                }
             </section>
         );
     }
