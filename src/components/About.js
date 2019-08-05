929eab06d089889ad5318d79fa0823391305979a
Brad Murphy <brad@blackairplane.com>
2019-08-05 01:42:40 -0400

    updating copy

--- src/components/About.js
+++ src/components/About.js
@@ -23,7 +23,7 @@ class About extends Component {
                 </p>
                 <p className="typography__copy typography__copy--light">
                     Aside from programming, I tend to spend my time either playing a never ending list of video game titles or hanging out
-                    with my dogs, Atlas & Siva. When we're not walking the city, you can usually find us grabbing some good eats or crashed out on the couch watching a the next netflix binge. And of course, last but not least -- jamming out to some good tunes!
+                    with my dogs, Atlas & Siva. When we're not walking the city, you can usually find us grabbing some good eats or crashed out on the couch watching the next thing to binge on Netflix. And of course, last but not least -- jamming out to some good tunes!
                 </p>
                 <p className="typography__copy typography__copy--light">
                     This is where I keep select digital creations that I find fun, unique and/or interesting. Feel free to explore at your leisure.
